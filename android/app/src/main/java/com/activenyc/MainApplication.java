60dd5be44b871e24aba2aae3f35f7fb67e59f02c
John Kneafsey <johnfkneafsey@gmail.com>
2017-04-02 14:12:46 -0400

    video added

--- android/app/src/main/java/com/activenyc/MainApplication.java
+++ android/app/src/main/java/com/activenyc/MainApplication.java
@@ -3,6 +3,7 @@ package com.activenyc;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import com.magus.fblogin.FacebookLoginPackage;
 import com.reactnative.photoview.PhotoViewPackage;
 import cl.json.RNSharePackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
             new FacebookLoginPackage(),
             new PhotoViewPackage(),
             new RNSharePackage(),
